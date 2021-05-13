5a8c42f35a064108fedba8facf889a8c22d959f7
Simon Lindholm <simon.lindholm10@gmail.com>
2021-05-14 01:09:13 +0200

    Consistent behavior between interp modes

--- r4300/cop1_d.c
+++ r4300/cop1_d.c
@@ -38,8 +38,6 @@
     do { if (emulate_float_crashes && !isnormal(x) && (x) != 0 && !isinf(x)) { \
         printf("Operation on denormal/nan: %lf; PC = 0x%lx\n", x, PC->addr); \
         stop=1; \
-        PC++; \
-        return; \
     } } while (0)
 
 #define CHECK_OUTPUT(x) \
@@ -47,15 +45,12 @@
         if (isnan(x)) { \
             printf("Invalid float operation; PC = 0x%lx\n", PC->addr); \
             stop=1; \
-            PC++; \
-            return; \
-        } else { \
-            /* Flush denormals to zero manually, since x87 doesn't have a built-in */ \
-            /* way to do it. Typically this doesn't matter, because denormals are */ \
-            /* too small to cause visible console/emu divergences, but since we */ \
-            /* check for them on entry to each operation this becomes important.. */ \
-            x = 0; \
         } \
+        /* Flush denormals to zero manually, since x87 doesn't have a built-in */ \
+        /* way to do it. Typically this doesn't matter, because denormals are */ \
+        /* too small to cause visible console/emu divergences, but since we */ \
+        /* check for them on entry to each operation this becomes important.. */ \
+        x = 0; \
     } } while (0)
 
 void ADD_D()
--- r4300/pure_interp.c
+++ r4300/pure_interp.c
@@ -46,8 +46,6 @@
     do { if (emulate_float_crashes && !isnormal(x) && (x) != 0 && !isinf(x)) { \
         printf("Operation on denormal/nan: %lf; PC = 0x%lx\n", x, interp_addr); \
         stop=1; \
-        interp_addr+=4; \
-        return; \
     } } while (0)
 
 #define CHECK_OUTPUT(x) \
@@ -55,15 +53,12 @@
         if (isnan(x)) { \
             printf("Invalid float operation; PC = 0x%lx\n", interp_addr); \
             stop=1; \
-            interp_addr+=4; \
-            return; \
-        } else { \
-            /* Flush denormals to zero manually, since x87 doesn't have a built-in */ \
-            /* way to do it. Typically this doesn't matter, because denormals are */ \
-            /* too small to cause visible console/emu divergences, but since we */ \
-            /* check for them on entry to each operation this becomes important.. */ \
-            x = 0; \
         } \
+        /* Flush denormals to zero manually, since x87 doesn't have a built-in */ \
+        /* way to do it. Typically this doesn't matter, because denormals are */ \
+        /* too small to cause visible console/emu divergences, but since we */ \
+        /* check for them on entry to each operation this becomes important... */ \
+        x = 0; \
     } } while (0)
 
 #ifdef DBG
