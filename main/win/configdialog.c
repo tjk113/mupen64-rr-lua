2a4b65aa26ae90301d3d1eddff2ceb4d0014afe0
tjk113 <tjkuhl1@live.com>
2022-06-07 04:48:47 -0400

    fixed gui error after exiting hotkey settings

--- main/win/configdialog.c
+++ main/win/configdialog.c
@@ -1287,7 +1287,7 @@ inithotkeysdialog:
 		SetDlgItemHotkey(hwnd, IDC_HOT_SPEEDDOWN, &Config.hotkey[2]);
 		SetDlgItemHotkeyAndMenu(hwnd, IDC_HOT_FRAMEADVANCE, &Config.hotkey[3], GetSubMenu(GetMenu(mainHWND),1), 1);
 		SetDlgItemHotkeyAndMenu(hwnd, IDC_HOT_PAUSE, &Config.hotkey[4], GetSubMenu(GetMenu(mainHWND),1), 0);
-        	SetDlgItemHotkeyAndMenu(hwnd, IDC_HOT_READONLY, &Config.hotkey[5], GetSubMenu(GetMenu(mainHWND),3), 5);
+        	SetDlgItemHotkeyAndMenu(hwnd, IDC_HOT_READONLY, &Config.hotkey[5], GetSubMenu(GetMenu(mainHWND),3), 15);
 		SetDlgItemHotkeyAndMenu(hwnd, IDC_HOT_PLAY, &Config.hotkey[6], GetSubMenu(GetMenu(mainHWND),3), 3);
 		SetDlgItemHotkeyAndMenu(hwnd, IDC_HOT_PLAYSTOP, &Config.hotkey[7], GetSubMenu(GetMenu(mainHWND),3), 4);
 		SetDlgItemHotkeyAndMenu(hwnd, IDC_HOT_RECORD, &Config.hotkey[8], GetSubMenu(GetMenu(mainHWND),3), 0);
