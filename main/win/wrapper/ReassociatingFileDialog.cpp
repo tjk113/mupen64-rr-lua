36feec460fed084adbfb63f5bfb695900243edf5
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-01-06 12:39:15 +0100

    Implement reassociating folder dialog for config dialog

--- main/win/wrapper/ReassociatingFileDialog.cpp
+++ main/win/wrapper/ReassociatingFileDialog.cpp
@@ -36,13 +36,13 @@ bool ReassociatingFileDialog::ShowFileDialog(char* path, const wchar_t* fTypes, 
 
 	IShellItem* shlPtr;
 	if (path[0] != NULL) {
-		printf("[ReassociatingFileDialog]: found path \"%s\"\n", path);
+		printf("[ReassociatingFileDialog]: Defaulting to path \"%s\"...\n", path);
 		if (SHCreateItemFromParsingName(convertCharArrayToLPCWSTR(path), NULL, IID_PPV_ARGS(&shlPtr)) != S_OK)
 			printf("[ReassociatingFileDialog]: Unable to create IShellItem from parsing path name");
 	}
 	else {
-		printf("[ReassociatingFileDialog]: found lastPath \"%s\"\n", (char*)lastPath);
-		if (SHCreateItemFromParsingName(lastPath, NULL, IID_PPV_ARGS(&shlPtr)) != S_OK)
+		printf("[ReassociatingFileDialog]: Restoring path \"%ls\"...\n", m_lastPathWide);
+		if (SHCreateItemFromParsingName(m_lastPathWide, NULL, IID_PPV_ARGS(&shlPtr)) != S_OK)
 			printf("[ReassociatingFileDialog]: Unable to create IShellItem from parsing lastPath name");
 	}
 	FAILSAFE(pFileDialog->SetFolder(shlPtr));
@@ -52,7 +52,7 @@ bool ReassociatingFileDialog::ShowFileDialog(char* path, const wchar_t* fTypes, 
 	FAILSAFE(pShellItem->GetDisplayName(SIGDN_FILESYSPATH, &pFilePath));
 
 	wcstombs(path, pFilePath, MAX_PATH);
-	wcsncpy(lastPath, pFilePath, MAX_PATH);
+	wcsncpy(m_lastPathWide, pFilePath, MAX_PATH);
 	
 	succeeded = true;
 
@@ -63,6 +63,95 @@ cleanUp:
 
 	return succeeded && strlen(path) > 0;
 }
+
+bool ReassociatingFileDialog::ShowFolderDialog(char* path, int maxSize, HWND hWnd)
+{
+    bool ret = false;
+    IFileDialog* pfd;
+    if (SUCCEEDED(CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pfd))))
+    {
+        if (m_lastPathShort && strlen(m_lastPathShort) > 1)
+        {
+            printf("[ReassociatingFileDialog]: Restoring path \"%s\"...\n", m_lastPathShort);
+            PIDLIST_ABSOLUTE pidl;
+            WCHAR wstarting_dir[MAX_PATH];
+            WCHAR* wc = wstarting_dir;
+            for (const char* c = m_lastPathShort; *c && wc - wstarting_dir < MAX_PATH - 1; ++c, ++wc)
+            {
+                *wc = *c == '/' ? '\\' : *c;
+            }
+            *wc = 0;
+
+            HRESULT hresult = ::SHParseDisplayName(wstarting_dir, 0, &pidl, SFGAO_FOLDER, 0);
+            if (SUCCEEDED(hresult))
+            {
+                IShellItem* psi;
+                hresult = ::SHCreateShellItem(NULL, NULL, pidl, &psi);
+                if (SUCCEEDED(hresult))
+                {
+                    pfd->SetFolder(psi);
+                }
+                ILFree(pidl);
+            }
+        }
+        
+        DWORD dwOptions;
+        if (SUCCEEDED(pfd->GetOptions(&dwOptions)))
+        {
+            pfd->SetOptions(dwOptions | FOS_PICKFOLDERS | FOS_PATHMUSTEXIST);
+        }
+        if (SUCCEEDED(pfd->Show(hWnd)))
+        {
+            IShellItem* psi;
+            if (SUCCEEDED(pfd->GetResult(&psi)))
+            {
+                WCHAR* tmp;
+                if (SUCCEEDED(psi->GetDisplayName(SIGDN_DESKTOPABSOLUTEPARSING, &tmp)))
+                {
+                    char* c = path;
+                    while (*tmp != NULL && (c - path) < (maxSize - 1))
+                    {
+                        *c = (char)*tmp;
+                        ++c;
+                        ++tmp;
+                    }
+                    *c = '\0';
+                    ret = true;
+                }
+                psi->Release();
+            }
+        }
+        pfd->Release();
+    }
+    
+    
+    // HACK: fail if non-filesystem folders were picked
+    if (ret && path)
+    {
+        bool containsShellIdentifiers = false;
+        for (size_t i = 0; i < maxSize; i++)
+        {
+            if (path[i] == '{' || path[i] == '}')
+            {
+                containsShellIdentifiers = true;
+                break;
+            }
+        }
+        if (containsShellIdentifiers)
+        {
+            printf("[ReassociatingFileDialog]: Special folder was picked!\n");
+            ret = false;
+        }
+    }
+
+    if (ret && path)
+    {
+        strcpy(m_lastPathShort, path);
+    }
+
+    return ret;
+}
+
 
 char* GetDefaultFileDialogPath(char* buf, char* path) { // buf must be MAX_PATH or greater
 	std::filesystem::path fullPath = path;
