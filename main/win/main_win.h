238e6ced0c2dd2ef0a7a99a751d6d6995c5bda67
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-05-24 20:17:11 +0200

    Bump version

--- main/win/main_win.h
+++ main/win/main_win.h
@@ -21,7 +21,7 @@
 #ifndef MAIN_WIN_H
 #define MAIN_WIN_H
 
-#define MUPEN_VERSION     "Mupen 64 1.1.0"
+#define MUPEN_VERSION     "Mupen 64 1.1.1"
 
 extern BOOL CALLBACK CfgDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam);
 extern void ShowMessage(const char* lpszMessage) ;
