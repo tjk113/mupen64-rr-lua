3128eac71ea39eb068a6e27d5f5994ae13a78f2e
Aurumaker72 <armkdev72@gmail.com>
2024-02-07 19:39:42 +0100

    Messenger: Add messenger

---
+++ main/messenger.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include <any>
+#include <functional>
+
+namespace Messenger
+{
+	/**
+	 * \brief Types of messages
+	 */
+	enum class Message
+	{
+		/**
+		 * \brief The emulator launched state has changed
+		 */
+		EmuLaunchedChanged,
+	};
+
+	/**
+	 * \brief Initializes the messenger
+	 */
+	void init();
+
+	/**
+	 * \brief Broadcasts a message to all listeners
+	 * \param message The message type
+	 * \param data The message data
+	 */
+	void broadcast(Message message, std::any data);
+
+	/**
+	 * \brief Subscribe to a message
+	 * \param message The message type to listen for
+	 * \param callback The callback to be invoked upon receiving the specified message type
+	 * \return A function which, when called, unsubscribes from the message
+	 */
+	std::function<void()> subscribe(Message message, std::function<void(std::any)> callback);
+}
