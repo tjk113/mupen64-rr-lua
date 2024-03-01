2f214247d6e6b0421d61a553eda9dfac1074b3ec
Aurumaker72 <armkdev72@gmail.com>
2024-03-01 18:27:10 +0100

    Messenger: Allow concurrent broadcasts
    Solves some deadlocks

--- main/messenger.cpp
+++ main/messenger.cpp
@@ -1,29 +1,23 @@
 ﻿#include "messenger.h"
 
-#include <Windows.h>
-
 namespace Messenger
 {
 	// List of subscribers: pair of uid, message type, and callback
 	std::vector<std::tuple<Message, std::function<void(std::any)>>> subscribers;
 
-	CRITICAL_SECTION cs;
 
 	void init()
 	{
-		InitializeCriticalSection(&cs);
 	}
 
 	void broadcast(Message message, std::any data)
 	{
-		EnterCriticalSection(&cs);
 		for (auto subscriber : subscribers)
 		{
 			if (std::get<0>(subscriber) != message)
 			 continue;
 			std::get<1>(subscriber)(data);
 		}
-		LeaveCriticalSection(&cs);
 	}
 
 	std::function<void()> subscribe(Message message,
@@ -31,7 +25,6 @@ namespace Messenger
 	{
 		auto index = subscribers.size();
 		subscribers.emplace_back(message, callback);
-
 		return [index]
 		{
 			subscribers.erase(subscribers.begin() + index);
