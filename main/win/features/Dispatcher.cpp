f4039ec19e721734fc8b40b9974bbe6fcbf18181
Aurumaker72 <armkdev72@gmail.com>
2024-03-06 17:23:11 +0100

    View: Fix dispatcher race condition

--- main/win/features/Dispatcher.cpp
+++ main/win/features/Dispatcher.cpp
@@ -1,13 +1,17 @@
 ﻿#include "Dispatcher.h"
+
+#include <mutex>
 #include <Windows.h>
 #include "../main_win.h"
 
 namespace Dispatcher
 {
 	std::optional<std::function<void()>> current_function;
+	std::mutex dispatcher_mutex;
 
 	void invoke(const std::function<void()>& func)
 	{
+		std::lock_guard lock(dispatcher_mutex);
 		current_function = std::make_optional(func);
 		SendMessage(mainHWND, WM_EXECUTE_DISPATCHER, 0, 0);
 	}
