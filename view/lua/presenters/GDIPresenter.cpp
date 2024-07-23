a6e93d45fc0156caf4c4c6e13c332a74d381c039
Aurumaker72 <armkdev72@gmail.com>
2024-07-23 12:15:25 +0200

    Lua/Presenters: Document GDIPresenter.cpp

--- view/lua/presenters/GDIPresenter.cpp
+++ view/lua/presenters/GDIPresenter.cpp
@@ -20,15 +20,18 @@ bool GDIPresenter::init(HWND hwnd)
 	GetClientRect(hwnd, &rect);
 	m_size = {(UINT32)rect.right - rect.left, (UINT32)rect.bottom - rect.top};
 
+	// 1. Create a back-buffer DC pointed to a bitmap
 	auto gdi_dc = GetDC(hwnd);
 	m_gdi_back_dc = CreateCompatibleDC(gdi_dc);
 	m_gdi_bmp = CreateCompatibleBitmap(gdi_dc, m_size.width, m_size.height);
 	SelectObject(m_gdi_back_dc, m_gdi_bmp);
 	ReleaseDC(hwnd, gdi_dc);
 
+	// 2. Make the provided window (which must have WS_EX_LAYERED) mask out our clear color, then fill the back DC with that color (effectively clearing it)
 	SetLayeredWindowAttributes(hwnd, lua_gdi_color_mask, 0, LWA_COLORKEY);
 	FillRect(m_gdi_back_dc, &rect, alpha_mask_brush);
 
+	// 3. Create a D2D1 RT and point it to our back DC
 	D2D1_RENDER_TARGET_PROPERTIES props =
 		D2D1::RenderTargetProperties(
 			D2D1_RENDER_TARGET_TYPE_DEFAULT,
@@ -67,6 +70,7 @@ void GDIPresenter::end_present()
 	auto main_dc = GetDC(m_hwnd);
 
 	m_d2d_render_target->EndDraw();
+	// We want BitBlt, not TransparentBlt because of mask color preservation. Those are dropped at composite-time and not blit-time.
 	BitBlt(main_dc, 0, 0, m_size.width, m_size.height, m_gdi_back_dc, 0, 0, SRCCOPY);
 
 	ReleaseDC(m_hwnd, main_dc);
