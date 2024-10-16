a67d99caced61ef705492bf6114b6184fceed3a8
Aurumaker72 <armkdev72@gmail.com>
2024-10-16 23:20:00 +0200

    Messenger: Fix race condition

--- shared/Messenger.cpp
+++ shared/Messenger.cpp
@@ -33,10 +33,31 @@ namespace Messenger
     // Whether a message is currently being broadcasted. Used to wait when subscribing.
     std::atomic g_broadcasting = 0;
 
+    // Whether a subscription is currently happening. Used to wait when broadcasting.
+    std::atomic g_subscribing = 0;
+
+    void wait_for_broadcast_end()
+    {
+        while (g_broadcasting != 0)
+        {
+            std::println("[Messenger] Waiting for broadcast end...");
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+    }
+
+    void wait_for_subscribe_end()
+    {
+        while (g_subscribing != 0)
+        {
+            std::println("[Messenger] Waiting for subscribe end...");
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+    }
+    
     void init()
     {
     }
-
+    
     /**
      * Rebuilds the subscriber cache from the current subscriber vector.
      */
@@ -52,6 +73,8 @@ namespace Messenger
 
     void broadcast(const Message message, std::any data)
     {
+        wait_for_subscribe_end();
+        
         ++g_broadcasting;
 
         for (const auto& subscriber : g_subscriber_cache[message])
@@ -61,27 +84,35 @@ namespace Messenger
 
         --g_broadcasting;
     }
-
+    
     std::function<void()> subscribe(Message message, t_user_callback callback)
     {
-        while (g_broadcasting != 0)
-        {
-            std::println("[Messenger] Waiting for broadcast to finish before subscribing...");
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        }
+        wait_for_broadcast_end();
+        wait_for_subscribe_end();
+
+        ++g_subscribing;
         
         Subscriber subscriber = {g_uid_accumulator++, callback};
 
         g_subscribers.emplace_back(message, subscriber);
         rebuild_subscriber_cache();
 
+        --g_subscribing;
+        
         return [=]
         {
+            wait_for_broadcast_end();
+            wait_for_subscribe_end();
+            
+            ++g_subscribing;
+
             std::erase_if(g_subscribers, [=](const auto& pair)
             {
                 return pair.second.uid == subscriber.uid;
             });
             rebuild_subscriber_cache();
+
+            --g_subscribing;
         };
     }
 }
