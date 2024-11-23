8b745dd5ed86b7d04b2c4a5d17186aeca28a60d8
Aurumaker72 <armkdev72@gmail.com>
2024-11-23 14:16:53 +0100

    Lua: Fix parameter corruption in atwarpmodifystatuschanged

--- view/lua/LuaService.cpp
+++ view/lua/LuaService.cpp
@@ -148,10 +148,10 @@ namespace LuaService
     void call_warp_modify_status_changed(const int32_t status)
     {
         RET_IF_EMPTY;
-        g_main_window_dispatcher->invoke([&]
+        g_main_window_dispatcher->invoke([=]
         {
             invoke_callbacks_with_key_on_all_instances(
-                [&](lua_State* L)
+                [=](lua_State* L)
                 {
                     lua_pushinteger(L, status);
                     return lua_pcall(L, 1, 0, 0);
