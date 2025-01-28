4d17f3c6b091c506f92927510f6faf75802f0d23
Aurumaker72 <armkdev72@gmail.com>
2025-01-28 21:42:17 +0100

    Core/GCOP1/Dev: Use pragma include guard

--- src/core/r4300/x86/gcop1_helpers.h
+++ src/core/r4300/x86/gcop1_helpers.h
@@ -4,8 +4,7 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-#ifndef GCOP1_HELPERS_H
-#define GCOP1_HELPERS_H
+#pragma once
 
 void gencheck_float_input_valid(int32_t stackBase);
 void gencheck_float_output_valid();
@@ -13,5 +12,3 @@ void gencheck_float_conversion_valid();
 
 extern float largest_denormal_float;
 extern double largest_denormal_double;
-
-#endif // GCOP1_HELPERS_H
