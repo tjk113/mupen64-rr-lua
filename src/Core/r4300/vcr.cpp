8dfd4b6c30efac09423bba9afa470b2ad9b42f0e
tjk113 <tjkuhl1@live.com>
2025-03-04 16:08:49 -0500

    VCR/Dev: Call `loop_movie` callback before `core_vcr_start_playback` for cleanliness

--- src/Core/r4300/vcr.cpp
+++ src/Core/r4300/vcr.cpp
@@ -707,8 +707,8 @@ void vcr_handle_playback(int32_t index, core_buttons* input)
 
         if (g_core->cfg->is_movie_loop_enabled)
         {
-            core_vcr_start_playback(g_movie_path);
             g_core->callbacks.loop_movie();
+            core_vcr_start_playback(g_movie_path);
             return;
         }
 
